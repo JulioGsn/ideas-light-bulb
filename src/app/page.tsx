358173c55b233fa18e39b0ff00385fd20fa7d3d5
JulioGsn <juliogsn15@outlook.com>
2024-09-23 23:01:45 -0300

    First commit light bulb blinking hahaha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,90 @@
-import Image from "next/image";
+"use client";
+import { AiFillBulb, AiOutlineArrowRight } from "react-icons/ai";
+import { useState, useEffect } from "react";
+import "./styles.css";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const offColor = "text-yellow-100";
+  const onColor = "text-yellow-300";
+  const [bulb, setBulb] = useState(false);
+  const [color, setColor] = useState(offColor);
+  const [idea, setIdea] = useState("");
+  const [showIdeaInput, setShowIdeaInput] = useState(false);
+  const [newIdeas, setNewIdeas] = useState([
+    {
+      title: "Be cool",
+    },
+    {
+      title: "New app idea",
+    },
+    {
+      title: "Old app idea",
+    },
+  ]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const [int1, setInt1] = useState();
+  const [int2, setInt2] = useState();
+
+  const handleLighBulbClick = () => {
+    setShowIdeaInput(true);
+    setColor(onColor);
+  };
+
+  const handleNewIdea = () => {
+    setNewIdeas([...newIdeas, { title: idea }]);
+    setShowIdeaInput(false);
+    setColor(offColor);
+    setIdea("");
+  };
+
+  return (
+    <div className="grid grid-rows-[10px_1fr_100px] items-start justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <header></header>
+      <main className="flex flex-col gap-10 items-center">
+        <div
+          className="hover:cursor-pointer text-[20rem] w-[200px] h-[200px] flex items-center justify-center"
+          onClick={handleLighBulbClick}
+        >
+          <AiFillBulb className={`transition-color ${color}`} />
+        </div>
+        <div className="h-10 min-w-96 w-96">
+          <div className="flex gap-2 items-center">
+            <input
+              type="text"
+              disabled={!showIdeaInput}
+              className="border w-full border-2 rounded-lg h-10 px-2 text-md text-black"
+              onChange={(e) => setIdea(e.target.value)}
+              value={idea}
+              onKeyDown={(e) => {
+                if (e.keyCode === 13) {
+                  handleNewIdea();
+                }
+              }}
+              placeholder="New idea"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <button
+              type="button"
+              onClick={handleNewIdea}
+              disabled={!showIdeaInput}
+            >
+              <AiOutlineArrowRight className="text-gray-800 text-lg" />
+            </button>
+          </div>
+        </div>
+        <div className="max-h-[400px] overflow-auto w-full">
+          {newIdeas.map((i) => {
+            return (
+              <div
+                className="rounded-lg leading-7 text-slate-600"
+                key={i.title}
+              >
+                {i.title}
+              </div>
+            );
+          })}
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <footer></footer>
     </div>
   );
 }
